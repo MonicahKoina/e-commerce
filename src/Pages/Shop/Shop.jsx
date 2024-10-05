b0b040e63b199ef19422afd33f75977736ede49c
Monicah Koina <monicahkoina1@gmail.com>
2024-10-05 15:07:41 +0300

    Created an Api request

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -5,6 +5,24 @@ function Shop() {
     const[products, setProducts]=useState([]);
     const[load, setLoad]=useState(true);
     const[error, setError]=useState(null);
+    
+    const fetchItems = async () =>{
+        try{
+            const items = await fetch('');
+            if(!load.ok){
+                throw new Error("Can't Load page");
+            }
+            const products = await items.json();
+            setProducts(products);
+        }
+           catch(error){
+            setError(error.message);
+           }
+
+           finally{
+            setLoad(false);
+           }
+        };
   return (
     <div></div>
   )
