d2bd9146d17f4982e9346b27c121488b077b1e90
Monicah Koina <monicahkoina1@gmail.com>
2024-10-21 20:48:09 +0300

    added light and dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import Home from './Pages/Home'
 import Layout from './Components/Layout/Layout'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Shop from './Pages/Shop';
+import { useContext } from 'react';
+import { ThemeContext } from './context/Theme';
 
 function App() {
+  const [{theme, isDark}, toggleTheme] = useContext(ThemeContext)
+  console.log("theme", theme);
   return <>
+  <div style={{backgroundColor:theme.backgroundColor, color: theme.color}}>
+    <div> {isDark ? 'Dark' : 'Light' } </div>
+    <button
+    className='bg-yellow-200 rounded-lg p-2' onClick={toggleTheme} >Change Theme</button>
   <BrowserRouter>
   <Layout>
   <Routes>
@@ -15,6 +23,7 @@ function App() {
   </Routes>
   </Layout>
   </BrowserRouter>
+  </div>
   </>;
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { ThemeProvider } from './context/Theme.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider>
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
