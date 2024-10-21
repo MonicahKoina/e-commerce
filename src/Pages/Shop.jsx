13edd38959e1a82f074e2c0de4e9d11dc805115d
Monicah Koina <monicahkoina1@gmail.com>
2024-10-21 13:30:43 +0300

    shop-useEffect

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,23 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Shop() {
+    const[loading, setLoading]=useState(false);
+    const[data, setData]=useState([]);
+
+    useEffect(()=>{
+        setLoading(true)
+        fetch("https://fakestoreapi.com/products")
+        .then((res)=>res.json())
+        .then((data)=>{
+            console.log(data);
+            setData(data);
+        })
+        .catch((e)=>{
+            console.log("An error occured while fetching data", e);
+        })
+        .finally(()=>setLoading(false));
+    },[])
+
   return (
-    <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pt-12 gap-16 w-3/4 mx-10'>
-        <div className="bg-slate-200 p-4">
-            <img 
-            className="w-[40%] mx-auto"
-            src="/shoe.jpeg" alt="shoe" />
-            <div className='text-center py-4'>
-                <h1>Product title</h1>
-                <a href="#" className='block'>
-                    <span className='text-sm text-red-700'>Category</span>
-                </a>
-                <span className='block '>$<span text-md>15.88</span></span>
-                <button className='border-2 rounded-lg py-1 px-3 bg-yellow-300'>Add to cart</button>
-            </div>
-        </div>
-    </div>
-  )
+      <div className='grid grid-cols-1 md:grid-cols-3 pt-12 gap-16 w-3/4 mx-auto'>
+       {loading &&
+        <div>
+           <h1>Loading</h1>
+           </div>
+           }
+       {data.map(product => {
+           return (
+               <div key={product.id} className=" flex flex-col p-4 border-2 rounded-md border-yellow-100 mb-10">
+                   <img 
+                   className="w-[40%] h-[40%] mx-auto"
+                   src={product.image} alt={product.title} />
+                   <div className='text-center py-4 leading-9 h-[60%]'>
+                       <h1>{product.title}</h1>
+                       <a href="#" className=''>
+                           <span className='text-sm text-black'>{product.category}</span>
+                       </a>
+                       <p className=''>price:$<span text-md>{product.price}</span></p>
+                     </div> 
+                     <div className=' mx-auto my-8 '>
+                       <button className='rounded-lg py-1 px-3 bg-yellow-500 '>Add to cart</button>
+                        </div>
+               </div>
+           );
+       })}
+      </div>
+  );
 }
 
 export default Shop
